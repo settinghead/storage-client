8d93d0032ae87652d24397e89744f9057293e0f6
Alex Deaconu <alex.deaconu@risevision.com>
2014-01-08 16:26:05 -0500

    Bound select and delete actions
    Currently the pressing the buttons will just show an alert

--- war/js/controllers.js
+++ war/js/controllers.js
@@ -31,23 +31,55 @@ function initScope($scope, $rootScope) {
 
 	$scope.$watch('mediaFiles', function(items) {
 
-		var selectedCount = 0;
+		var checkedCount = 0;
 
 		items.forEach(function(item) {
-			if (item.selected) {
-				selectedCount++;
+			if (item.checked) {
+				checkedCount++;
 			}
 		});
 
-		$rootScope.$broadcast('SelectedCountChange', selectedCount);		
+		$rootScope.$broadcast('CheckedCountChange', checkedCount);		
 
 	}, true);
 
 	$scope.$watch('selectAll', function(v) {
 
     for ( var i = 0; i < $scope.mediaFiles.length; ++i ) {
-        $scope.mediaFiles[ i ].selected = v;
+        $scope.mediaFiles[ i ].checked = v;
     }
+
+	});
+
+	$rootScope.$on('FileSelectAction', function(event, file) {
+
+		if (!file) {
+
+		  for ( var i = 0; i < $scope.mediaFiles.length; ++i ) {
+        if ($scope.mediaFiles[ i ].checked) {
+					file = $scope.mediaFiles[ i ];
+				}
+			}
+	
+		}
+
+		if (file) {
+			alert(file.key + " selected!");
+		}
+
+	});
+
+	$rootScope.$on('FileDeleteAction', function(event) {
+
+		var selectedFiles = new Array();
+
+		for ( var i = 0; i < $scope.mediaFiles.length; ++i ) {
+			if ($scope.mediaFiles[ i ].checked) {
+				selectedFiles.push($scope.mediaFiles[ i ]);
+			}
+		}
+	
+		alert(selectedFiles.length + " files selected for deletion!");
 
 	});
 
@@ -59,7 +91,7 @@ function ButtonsController($scope, $rootScope) {
 	$scope.downloadDisabled = true;
 	$scope.deleteDisabled = true;	
 	
-	$rootScope.$on('SelectedCountChange', function(event, count)	 {
+	$rootScope.$on('CheckedCountChange', function(event, count)	 {
 
 		$scope.selectDisabled = count != 1;
 		$scope.downloadDisabled = !count;
