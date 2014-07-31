b9b5680191b6337f7121e986538a02b250693167
tejohnso <mail@tyler-johnson.ca>
2014-07-31 01:16:35 -0400

    Update bandwidth feature to not expect broadcasts

--- web/js/svc-bandwidth.js
+++ web/js/svc-bandwidth.js
@@ -1,38 +1,48 @@
 "use strict";
 angular.module("medialibrary")
-.factory("BandwidthService", ["$q", "storageAPILoader",
-  function ($q, storageAPILoader) {
-    var factory = {};
-    factory.oAuthReady = false;
-    factory.bandwidthValuesCache = {};
-    factory.getBandwidth = function (companyId) {
-      var deferred = $q.defer();
+.factory("BandwidthService", ["$q", "storageAPILoader", "OAuthService",
 
-      if (factory.bandwidthValuesCache.hasOwnProperty(companyId)) {
-        deferred.resolve(factory.bandwidthValuesCache[companyId]);
-        return deferred.promise;
-      }
+function bandwidthFactory($q, storageAPILoader, OAuthService) {
+  var service = {}
+     ,bandwidthValuesPromiseCache = {};
 
-      if (companyId) {
-        storageAPILoader.get().then(function (storageApi) {
-          var request = storageApi.getBucketBandwidth({
-            "companyId": companyId,
-          });
-          request.execute(function (resp) {
-            if (resp.result === false) {
-              deferred.resolve("undetermined");
-            } else {
-              resp.message = parseInt(resp.message, 10);
-              resp.message = isNaN(resp.message) ? 0 : resp.message / 1000000;
-              console.log("Setting bandwidth: " + resp.message);
-              factory.bandwidthValuesCache[companyId] = resp.message;
-              deferred.resolve(resp.message);
-            }
-          });
-        });
+  service.getBandwidth = function(companyId) {
+    if (!companyId) {return {};}
+
+    if (bandwidthValuesPromiseCache.hasOwnProperty(companyId)) {
+      return bandwidthValuesPromiseCache[companyId];
+    }
+
+    bandwidthValuesPromiseCache[companyId] =
+    $q.all([storageAPILoader.get(), OAuthService.getAuthStatus()])
+    .then(function(results) {
+      if (results[1] === false) {return;}
+      var request = results[0].getBucketBandwidth({
+        "companyId": companyId
+      });
+      return executeRequest(request);
+    }).then(function(resp) {
+      return resp;
+    });
+    return bandwidthValuesPromiseCache[companyId];
+  };
+
+  function executeRequest(request) {
+    var defer = $q.defer();
+    request.execute(function(resp) {
+      if (resp.result === false) {
+        defer.resolve("undetermined");
+      } else {
+        resp.message = parseInt(resp.message, 10);
+        resp.message = isNaN(resp.message) ? 0 : resp.message / 1000000;
+        resp.message = resp.message < 1 ?
+                       "less than one" : resp.message.toFixed(2);
+        console.log("Received bandwidth: " + resp.message);
+        defer.resolve(resp.message);
       }
-      return deferred.promise;
-    };
-    return factory;
+    });
+    return defer.promise;
   }
-]);
+
+  return service;
+}]);
